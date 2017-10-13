a1616bc969ee96223a7eb664a4281d23738b404a
Erick <rick.mutua12@gmail.com>
2017-10-13 17:44:18 +0300

    update

--- run.py
+++ run.py
@@ -48,3 +48,30 @@ def main():
     print("Hello, Welcome to your password locker, please login")
     user_name = input()
 
+    print(f"Hello {user_name}. What would you like to do?")
+    print('\n')
+
+    while True:
+        print("Use these short codes : cc - create a new credential, dc - display credentials, fc -find a credential, ex -exit")
+
+        short_code = input().lower()
+
+        if short_code == 'cc':
+            print("New Credential")
+            print("-"*10)
+
+            print("Account Name . . . .")
+            account_name = input()
+
+            print("Account Password . . . .")
+            account_password = input()
+
+            print("Account Email . . . .")
+            account_email = input()
+
+            save_credential(create_credential(account_name, account_password, account_email))
+
+            print('\n')
+
+            print(f"New Credential {account_name} {account_password} {account_email}")
+            
