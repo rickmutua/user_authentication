7a935f01fd02ca0e0529a64303d34a5b575ae921
Erick <rick.mutua12@gmail.com>
2017-10-13 12:29:11 +0300

    first commit

---
+++ test.py
@@ -0,0 +1,3 @@
+import unittest # importing the unnitest module
+
+from user import User # importing the user class
---
+++ user.py
@@ -0,0 +1,15 @@
+import sys
+
+class User:
+    '''
+    class that generates new instances of user authentication
+    '''
+
+    def __init__(self, user_name, password):
+        '''
+        method that helps us define properties for our objects
+        :param user_name:
+        :param password:
+        '''
+
+
