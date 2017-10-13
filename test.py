4c11e137b218eb8c83898ec0484b285e0506b848
Erick <rick.mutua12@gmail.com>
2017-10-13 12:47:48 +0300

    added class TestUser and defined it and also renamed property password to user password

--- test.py
+++ test.py
@@ -1,3 +1,29 @@
-import unittest # importing the unnitest module
+import unittest # importing the unittest module
 
 from user import User # importing the user class
+
+class TestUser(unittest.TestCase):
+    '''
+    test class that defines test cases for the user class behaviours
+    '''
+
+    def setUp(self):
+        '''
+        method to run before each test case
+        :return:
+        '''
+
+        self.new_user = User("Erick","Mutua1234")
+
+    def test_init(self):
+        '''
+        test case to test if an object is initialized properly
+        :return:
+        '''
+
+        self.assertEqual(self.new_user.user_name, "Erick")
+        self.assertEqual(self.new_user.user_password, "Mutua1234")
+
+
+if __name__ == '__main__':
+    unittest.main()
--- user.py
+++ user.py
@@ -5,11 +5,11 @@ class User:
     class that generates new instances of user authentication
     '''
 
-    def __init__(self, user_name, password):
+    def __init__(self, user_name, user_password):
         '''
         method that helps us define properties for our objects
         :param user_name:
-        :param password:
+        :param user_password:
         '''
 
 
